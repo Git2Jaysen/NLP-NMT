06bb52f47f40536708c3cdba371f0bf66ba85f35
Jaysen <mailbox_of_xxj@126.com>
2018-12-12 11:56:51 +0800

    modify config.json and model.py

--- model.py
+++ model.py
@@ -52,24 +52,117 @@ def input_fn(is_training, params):
     # return dataset
     return dataset
 
-def BiRNN_model_fn(features, labels, mode, params):
+def RNN_model_fn(features, labels, mode, params):
+    """the model is defined in (Bahdanau et al., 2015). This implementation
+    is referring to "Neural Machine Translation (seq2seq) Tutorial" on
+    Github, with a Bi-LSTM encoder and a LSTM decoder, using attention
+    mechanism.
+
+    Args:
+
+    Returns:
+    """
     # unzip source and target data
+    # src_sequences shape: [batch_size, max_sequence_lenght]
+    # src_lengths shape: [batch_size]
     src_sequences, src_lengths = features
+    # tgt_sequences shape: [batch_size, max_sequence_lenght]
+    # tgt_lengths shape: [batch_size]
     tgt_sequences, tgt_lengths = labels
     # define model graph
-
+    # the model is defined in (Bahdanau et al., 2015). This implementation is
+    # referring to "Neural Machine Translation (seq2seq) Tutorial" on Github,
+    # with a Bi-LSTM encoder and a LSTM decoder, using attention mechanism.
+    # encoder
+    with tf.variable_scope("RNN_encoder", reuse=reuse):
+        embedding_encoder = tf.get_variable(
+            "embedding_encoder",
+            [params["src_word_size"], params["embedding_size"]]
+        )
+        encoder_emb_inp = tf.nn.embedding_lookup(
+            embedding_encoder,
+            src_sequences
+        )
+        encoder_cell_fw = tf.nn.rnn_cell.LSTMCell(params["rnn_units"])
+        encoder_cell_bw = tf.nn.rnn_cell.LSTMCell(params["rnn_units"])
+        (output_fw, output_bw), (state_fw, state_bw) = (
+            tf.nn.bidirectional_dynamic_rnn(
+                encoder_cell_fw,
+                encoder_cell_bw,
+                encoder_emb_inp,
+                sequence_length=src_lengths,
+                dtype=tf.int32
+            )
+        )
+        encoder_outputs = tf.concat([output_fw, output_bw], axis=-1)
+        encoder_state = tf.concat([state_fw, state_bw], axis=-1)
+    # decoder
+    with tf.variable_scope("RNN_decoder", reuse=reuse):
+        embedding_decoder = tf.get_variable(
+            "embedding_decoder",
+            [params["tgt_word_size"], params["embedding_size"]]
+        )
+        decoder_emb_inp = tf.nn.embedding_lookup(
+            embedding_decoder,
+            tgt_sequences
+        )
+        # using for initializing decoder
+        decoder_cell = tf.nn.rnn_cell.LSTMCell(params["rnn_units"])
+        # map features to tgt_word_size(using for selecting words)
+        projection_layer = tf.layers.Dense(
+            params["tgt_word_size"],
+            use_bias=False
+        )
+        # different helper when training and testing
+        if mode == tf.estimator.ModeKeys.TRAIN:
+            helper = tf.contrib.seq2seq.TrainingHelper(
+                decoder_emb_inp,
+                tgt_lengths
+            )
+        else:
+            helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(
+                decoder_emb_inp,
+                tgt_lengths
+            )
+            maximum_iterations = tf.round(tf.reduce_max(source_lengths) * 2)
+        # build basic decoder
+        decoder = tf.contrib.seq2seq.BasicDecoder(
+            decoder_cell,
+            helper,
+            encoder_state,
+            output_layer=projection_layer
+        )
+        if mode == tf.estimator.ModeKeys.TRAIN:
+            decoder_outputs, _ = tf.contrib.seq2seq.dynamic_decode(decoder)
+        else:
+            decoder_outputs, _ = tf.contrib.seq2seq.dynamic_decode(
+                decoder,
+                maximum_iterations=maximum_iterations
+            )
     # define model behavior
     if (mode == tf.estimator.ModeKeys.TRAIN or
         mode == tf.estimator.ModeKeys.EVAL):
-        loss = ...
+        logits = decoder_outputs.rnn_output
+        crossent = tf.nn.sparse_softmax_cross_entropy_with_logits(
+            labels=decoder_outputs, logits=logits
+        )
+        loss = tf.reduce_sum(
+            crossent * target_weights / params["batch_size"]
+        )
     else:
         loss = None
     if mode == tf.estimator.ModeKeys.TRAIN:
-        train_op = ...
+        variables = tf.trainable_variables()
+        gradients = tf.gradients(loss, variables)
+        clipped_gradients, _ = tf.clip_by_global_norm(gradients,
+                                                      max_global_norm)
+        optimizer = tf.train.AdamOptimizer()
+        train_op = optimizer.apply_gradients(zip(clipped_gradients,
+                                                 variables))
     else:
         train_op = None
     if mode == tf.estimator.ModeKeys.PREDICT:
-        predictions = ...
+        predictions = decoder_outputs.sample_id
     else:
         predictions = None
     # return EstimatorSpec instance
